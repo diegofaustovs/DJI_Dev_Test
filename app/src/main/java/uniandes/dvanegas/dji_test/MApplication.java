ece402e1220751a742a9880fa9d37e39ced9fd78
Diego F. Vanegas <diegofaustovs@gmail.com>
2018-08-09 03:44:30 -0500

    Missions Tutorial - TouchFly|TrackFly

--- app/src/main/java/uniandes/dvanegas/dji_test/MApplication.java
+++ app/src/main/java/uniandes/dvanegas/dji_test/MApplication.java
@@ -4,13 +4,13 @@ import android.app.Application;
 import android.content.Context;
 import com.secneo.sdk.Helper;
 public class MApplication extends Application {
-    private FPVDemoApplication fpvDemoApplication;
+    private DJIDemoApplication fpvDemoApplication;
     @Override
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
         Helper.install(MApplication.this);
         if (fpvDemoApplication == null) {
-            fpvDemoApplication = new FPVDemoApplication();
+            fpvDemoApplication = new DJIDemoApplication();
             fpvDemoApplication.setContext(this);
         }
     }
--- app/src/main/java/uniandes/dvanegas/dji_test/MainActivity.java
+++ app/src/main/java/uniandes/dvanegas/dji_test/MainActivity.java
@@ -1,6 +1,7 @@
 package uniandes.dvanegas.dji_test;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -18,13 +19,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import dji.common.useraccount.UserAccountState;
+import dji.common.util.CommonCallbacks;
 import dji.log.DJILog;
+import dji.sdk.useraccount.UserAccountManager;
 import uniandes.dvanegas.dji_test.R;
 
 import java.util.ArrayList;
@@ -37,90 +45,291 @@ import dji.sdk.base.BaseComponent;
 import dji.sdk.base.BaseProduct;
 import dji.sdk.sdkmanager.DJISDKManager;
 
-public class MainActivity extends Activity implements TextureView.SurfaceTextureListener, View.OnClickListener {
-    protected TextureView mVideoSurface = null;
-    private Button mCaptureBtn, mShootPhotoModeBtn, mRecordVideoModeBtn;
-    private ToggleButton mRecordBtn;
-    private TextView recordingTime;
+public class MainActivity extends DemoBaseActivity {
+
+    public static final String TAG = MainActivity.class.getName();
+
+    public String mString = null;
+    private BaseProduct mProduct;
+    private ArrayList<DemoInfo> demos = new ArrayList<DemoInfo>();
+    private ListView mListView;
+    private DemoListAdapter mDemoListAdapter = new DemoListAdapter();
+    private TextView mFirmwareVersionView;
+
+    private static final String[] REQUIRED_PERMISSION_LIST = new String[]{
+            Manifest.permission.VIBRATE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.WAKE_LOCK,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE,
+    };
+    private List<String> missingPermission = new ArrayList<>();
+    private AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
+    private static final int REQUEST_PERMISSION_CODE = 125;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        initUI();
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
-    public void onReturn(View view){
-        this.finish();
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-    @Override
-    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-    }
-    @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-    }
-    @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-        return false;
-    }
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-    }
-    private void initUI() {
-        // init mVideoSurface
-        mVideoSurface = findViewById(R.id.video_previewer_surface);
-        recordingTime = findViewById(R.id.timer);
-        mCaptureBtn = findViewById(R.id.btn_capture);
-        mRecordBtn = findViewById(R.id.btn_record);
-        mShootPhotoModeBtn = findViewById(R.id.btn_shoot_photo_mode);
-        mRecordVideoModeBtn = findViewById(R.id.btn_record_video_mode);
+        checkAndRequestPermissions();
 
-        if (null != mVideoSurface) {
-            mVideoSurface.setSurfaceTextureListener(this);
+        setContentView(R.layout.activity_main);
+
+        mConnectStatusTextView = (TextView) findViewById(R.id.ConnectStatusTextView);
+
+        mListView = (ListView)findViewById(R.id.listView);
+        mListView.setAdapter(mDemoListAdapter);
+
+        mFirmwareVersionView = (TextView)findViewById(R.id.version_tv);
+
+        loadDemoList();
+
+        mDemoListAdapter.notifyDataSetChanged();
+
+        updateVersion();
+
+    }
+
+    /**
+     * Checks if there is any missing permissions, and
+     * requests runtime permission if needed.
+     */
+    private void checkAndRequestPermissions() {
+        // Check for permissions
+        for (String eachPermission : REQUIRED_PERMISSION_LIST) {
+            if (ContextCompat.checkSelfPermission(this, eachPermission) != PackageManager.PERMISSION_GRANTED) {
+                missingPermission.add(eachPermission);
+            }
+        }
+        // Request for missing permissions
+        if (!missingPermission.isEmpty() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,
+                    missingPermission.toArray(new String[missingPermission.size()]),
+                    REQUEST_PERMISSION_CODE);
         }
 
-        mCaptureBtn.setOnClickListener(this);
-        mRecordBtn.setOnClickListener(this);
-        mShootPhotoModeBtn.setOnClickListener(this);
-        mRecordVideoModeBtn.setOnClickListener(this);
-        recordingTime.setVisibility(View.INVISIBLE);
-        mRecordBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    }
+
+    /**
+     * Result of runtime permission request
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // Check for granted permission and remove from missing list
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            for (int i = grantResults.length - 1; i >= 0; i--) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    missingPermission.remove(permissions[i]);
+                }
+            }
+        }
+        // If there is enough permission, we will start the registration
+        if (missingPermission.isEmpty()) {
+            startSDKRegistration();
+        } else {
+            showToast("Missing permissions!!!");
+        }
+    }
+
+    private void startSDKRegistration() {
+        if (isRegistrationInProgress.compareAndSet(false, true)) {
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    showToast( "registering, pls wait...");
+                    DJISDKManager.getInstance().registerApp(getApplicationContext(), new DJISDKManager.SDKManagerCallback() {
+                        @Override
+                        public void onRegister(DJIError djiError) {
+                            if (djiError == DJISDKError.REGISTRATION_SUCCESS) {
+                                DJILog.e("App registration", DJISDKError.REGISTRATION_SUCCESS.getDescription());
+                                DJISDKManager.getInstance().startConnectionToProduct();
+                                showToast("Register Success");
+                            } else {
+                                showToast( "Register sdk fails, check network is available");
+                            }
+                            Log.v(TAG, djiError.getDescription());
+                        }
+
+                        @Override
+                        public void onProductDisconnect() {
+                            Log.d(TAG, "onProductDisconnect");
+                            showToast("Product Disconnected");
+
+                        }
+                        @Override
+                        public void onProductConnect(BaseProduct baseProduct) {
+                            Log.d(TAG, String.format("onProductConnect newProduct:%s", baseProduct));
+                            showToast("Product Connected");
+
+                        }
+                        @Override
+                        public void onComponentChange(BaseProduct.ComponentKey componentKey, BaseComponent oldComponent,
+                                                      BaseComponent newComponent) {
+
+                            if (newComponent != null) {
+                                newComponent.setComponentListener(new BaseComponent.ComponentListener() {
+
+                                    @Override
+                                    public void onConnectivityChange(boolean isConnected) {
+                                        Log.d(TAG, "onComponentConnectivityChanged: " + isConnected);
+                                    }
+                                });
+                            }
+                            Log.d(TAG,
+                                    String.format("onComponentChange key:%s, oldComponent:%s, newComponent:%s",
+                                            componentKey,
+                                            oldComponent,
+                                            newComponent));
+
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    private void showToast(final String toastMsg) {
+        runOnUiThread(new Runnable() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            public void run() {
+                Toast.makeText(getApplicationContext(), toastMsg, Toast.LENGTH_LONG).show();
 
             }
         });
     }
+
+    private void loadDemoList() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View v, int index, long arg3) {
+                onListItemClick(index);
+            }
+        });
+        demos.clear();
+        demos.add(new DemoInfo(R.string.title_activity_tracking_test, R.string.demo_desc_tracking, TrackingTestActivity.class));
+        demos.add(new DemoInfo(R.string.title_activity_pointing_test, R.string.demo_desc_pointing, PointingTestActivity.class));
+    }
+
+    private void onListItemClick(int index) {
+        Intent intent = null;
+        intent = new Intent(MainActivity.this, demos.get(index).demoClass);
+        this.startActivity(intent);
+    }
+
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_capture:{
-                break;
-            }
-            case R.id.btn_shoot_photo_mode:{
-                break;
-            }
-            case R.id.btn_record_video_mode:{
-                break;
-            }
-            default:
-                break;
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void onReturn(View view){
+        Log.d(TAG ,"onReturn");
+        this.finish();
+    }
+
+    @SuppressLint("ViewHolder")
+    private class DemoListAdapter extends BaseAdapter {
+        public DemoListAdapter() {
+            super();
+        }
+
+        @Override
+        public View getView(int index, View convertView, ViewGroup parent) {
+            convertView = View.inflate(MainActivity.this, R.layout.demo_info_item, null);
+            TextView title = (TextView)convertView.findViewById(R.id.title);
+            TextView desc = (TextView)convertView.findViewById(R.id.desc);
+
+            title.setText(demos.get(index).title);
+            desc.setText(demos.get(index).desc);
+            return convertView;
+        }
+        @Override
+        public int getCount() {
+            return demos.size();
+        }
+        @Override
+        public Object getItem(int index) {
+            return  demos.get(index);
+        }
+
+        @Override
+        public long getItemId(int id) {
+            return id;
         }
     }
 
+    private static class DemoInfo{
+        private final int title;
+        private final int desc;
+        private final Class<? extends android.app.Activity> demoClass;
+
+        public DemoInfo(int title , int desc,Class<? extends android.app.Activity> demoClass) {
+            this.title = title;
+            this.desc  = desc;
+            this.demoClass = demoClass;
+        }
+    }
+
+    @Override
+    protected void onProductChange() {
+        super.onProductChange();
+        loadDemoList();
+        mDemoListAdapter.notifyDataSetChanged();
+        updateVersion();
+        loginAccount();
+    }
+
+    private void setResultToToast(final String string) {
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loginAccount(){
+
+        UserAccountManager.getInstance().logIntoDJIUserAccount(this, new CommonCallbacks.CompletionCallbackWith<UserAccountState>() {
+            @Override
+            public void onSuccess(UserAccountState userAccountState) {
+                Log.d(TAG ,"Login Success");
+            }
+
+            @Override
+            public void onFailure(DJIError djiError) {
+                setResultToToast("Login Failed: " +  djiError.getDescription());
+            }
+        });
+    }
+
+    String version = null;
+
+    private void updateVersion() {
+
+        BaseProduct product = DJISDKManager.getInstance().getProduct();
+        if(product != null) {
+            version = product.getFirmwarePackageVersion();
+        }
+
+        if(version == null) {
+            version = "N/A";
+        }
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mFirmwareVersionView.setText("Firmware version: " + version);
+            }
+        });
+
+    }
 }
